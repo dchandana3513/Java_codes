70cb30ba3afcd08d90eaf17e90b87e46f98f964a
dchandana3513 <118003212+dchandana3513@users.noreply.github.com>
2024-09-01 14:31:32 +0530

    Create DecodeString.java

---
+++ DecodeString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Decodestring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String res = "";
+        int count = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == '0' && count == 0) {
+                count = 0;
+            } else if (c == '0' && count > 0) {
+                res += (char)('A' + count - 1);
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+        
+        if (s.charAt(s.length() - 1) == '1') {
+            res += (char)('A' + count - 1);
+        }
+        
+        System.out.println(res);
+    }
+}
